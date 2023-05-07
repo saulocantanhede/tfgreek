aaef096af0504d7e1894658add8d93965bbabe49
saulocantanhede <121267518+saulocantanhede@users.noreply.github.com>
2023-05-07 18:06:50 -0400

    Add files via upload

---
+++ tf/0.4/note.tf
@@ -0,0 +1,15 @@
+@node
+@author=Evangelists and apostles
+@converters=Dirk Roorda et al. (Text-Fabric)
+@description=this is XML attribute note
+@descriptionTf=Nestle 1904 edition
+@institute=ETCBC (Eep Talstra Centre for Bible and Computer)
+@language=nl
+@sourceFormat=XML
+@title=Greek New Testament
+@valueType=str
+@version=0.4
+@writtenBy=Text-Fabric
+@dateWritten=2023-05-07T21:39:48Z
+
+337283	discontinuous discourse
---
+++ tf/0.4/otext.tf
@@ -0,0 +1,15 @@
+@config
+@author=Evangelists and apostles
+@converters=Dirk Roorda et al. (Text-Fabric)
+@descriptionTf=Nestle 1904 edition
+@fmt:text-orig-full={text}{after}
+@institute=ETCBC (Eep Talstra Centre for Bible and Computer)
+@language=nl
+@sectionFeatures=book_short,chapter,verse
+@sectionTypes=book,chapter,verse
+@sourceFormat=XML
+@title=Greek New Testament
+@version=0.4
+@writtenBy=Text-Fabric
+@dateWritten=2023-05-07T21:39:55Z
+
